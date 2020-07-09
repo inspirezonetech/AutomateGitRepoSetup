13c2e53483c78ec982763ffb40143ae87a15471d
fum <->
2020-07-09 07:34:01 +0100

    Add option to use token instead of password - token is read from file

--- automate-git.py
+++ automate-git.py
@@ -73,7 +73,7 @@ def commit_files(directory):
 
 #--------------- remote setup ---------------#
 
-def create_github_repo(repo_name):
+def create_github_repo(repo_name, auth_type):
 
     #ask for user github account name
     github_name  = input("Enter your github user name: ")
@@ -81,10 +81,21 @@ def create_github_repo(repo_name):
     #generate data for request, set repo to private
     repo_config = '{"name": "%s", "private": "true"}' %repo_name
 
-    response = subprocess.run(["curl", "--user", github_name, "--data", repo_config, "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
+    #create repo
+    if auth_type == '1':
+        access_token = read_token_from_file()
+        header = 'Authorization: token ' + access_token
+        response = subprocess.run(["curl", "--header", header, "--data", repo_config, "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
+    
+    else:
+        response = subprocess.run(["curl", "--user", github_name, "--data", repo_config, "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
 
     #confirm repo now exists under user
-    response = subprocess.run(["curl", "--user", github_name, "--request", "GET", "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
+    if auth_type == '1':
+        response = subprocess.run(["curl", "--header", header, "--request", "GET", "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
+    
+    else:
+        response = subprocess.run(["curl", "--user", github_name, "--request", "GET", "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
     
     #convert from completed process for easier parsing
     response_json = json.loads(response.stdout.decode("utf-8"))
@@ -104,45 +115,90 @@ def create_github_repo(repo_name):
     return github_name, remote_url
 
 
-
-
 #--------------- link local and remote ---------------#
 
-def add_remote_repo_url(directory, remote_url):
+def add_remote_repo_url(directory, github_name, remote_url, auth_type):
 
     #url for repo
     server = remote_url + ".git"
     print("server: %s" %server)
+    
+    if auth_type == '1':
+        access_token = read_token_from_file()
+        server = server.replace("//", "//%s:%s@" %(github_name, access_token))
 
-    #add as origin
-    subprocess.run(["git", "remote", "add", "origin", server], cwd=directory)
+    else:
+        server = server.replace("//", "//%s@" %(github_name))
+
+    print("server: %s" %server)
+
+    #set origin
+    subprocess.run(["git", "remote", "set-url", "origin", server], cwd=directory)
+
     pass
     return server
 
 
-def push_local_repo_to_remote(directory, github_name, server):
+def push_local_repo_to_remote(directory, server):
 
     #insert username to origin url
-    push_url = server.replace("//", "//%s@" %github_name)
-    print("repo link: %s" %push_url)
+    push_url = server
+    print("push url: %s" %push_url)
 
     #push to server
     subprocess.run(["git", "push", "-u", push_url, "master"], cwd=directory)
     pass
     
 
+def remote_auth_option():
+
+    print("Select authentication type to github")
+    auth_type = input("Personal Access Token (enter 1) or Password (enter 2)?: ") 
+
+    if auth_type == '1':
+        print("Using Token")
+        
+    else:
+        print("Using Password")
+
+    pass
+    return auth_type
+
+
+def read_token_from_file():
+
+    TOKEN_LEN = 40
+    
+    #read token from file
+    with open('token', 'r') as token_file:
+        token = token_file.read(TOKEN_LEN)
+        print("Access token found: %s" %token)
+
+    return token
 
 
 def start_program_flow():
     
     print("------ Start ------")
+
+    r_auth_type = remote_auth_option()
+
     r_directory, r_repo_name = create_local_git_repo()
+
     r_first_file = create_local_repo_file(r_directory)
+
     add_files_for_commit(r_directory, r_first_file)
+
     commit_files(r_directory)
-    r_github_name, r_remote_url = create_github_repo(r_repo_name)
-    r_server = add_remote_repo_url(r_directory, r_remote_url)
-    push_local_repo_to_remote(r_directory, r_github_name, r_server)
+
+    r_github_name, r_remote_url = create_github_repo(r_repo_name, r_auth_type)
+
+    r_server = add_remote_repo_url(r_directory, r_github_name, r_remote_url, r_auth_type)
+
+    push_local_repo_to_remote(r_directory, r_server)
+
+    pass
+
 
 def main():
     start_program_flow()
