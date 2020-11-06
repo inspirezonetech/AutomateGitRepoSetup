27d5d4d6fc5338b7e7e80a53892fa1841c83838f
fum <66768334+funbeedev@users.noreply.github.com>
2020-11-06 07:46:20 +0000

    tidy up and apply flake8 linting

--- automate-git.py
+++ automate-git.py
@@ -1,5 +1,3 @@
-#Init a local git repo, create remote repo using github API and push local to remote. prompts for user input
-
 import subprocess
 import json
 from pathlib import Path
@@ -16,30 +14,29 @@ access_token = config.get('your_settings', 'your_token')
 repo_name = config.get('your_settings', 'repo_name')
 first_file = config.get('your_settings', 'commit_file')
 msg = config.get('your_settings', 'first_commit_msg')
-github_name  = config.get('your_settings', 'github_name')
+github_name = config.get('your_settings', 'github_name')
 pc_directory = config.get('your_settings', 'directory')
 repo_directory = pc_directory + '/' + repo_name
 
-#--------------- local set up ---------------#
 
 def create_local_git_repo():
 
-    #create directory if it doesn't exist
+    # create directory if it doesn't exist
     check_dir = Path(pc_directory)
     if check_dir.exists():
         print("Directory exists. Skip create directory")
     else:
         subprocess.run(["mkdir", pc_directory])
 
-    print("You selected repo %s" %repo_directory)
+    print("You selected repo %s" % repo_directory)
 
-    #create repo folder if it doesn't exist
+    # create repo folder if it doesn't exist
     check_repo = Path(repo_directory)
     if check_repo.exists():
         print("Repo already exists. Skip git init")
     else:
         subprocess.run(["mkdir", repo_directory])
-        #init repo
+        # init repo
         subprocess.run(["git", "init"], cwd=repo_directory)
 
     pass
@@ -49,11 +46,11 @@ def create_local_repo_file():
 
     file_path = repo_directory + "/" + first_file
     print(file_path)
-    
-    #create file if it doesn't exist
+
+    # create file if it doesn't exist
     check_file = Path(file_path)
     if check_file.exists():
-        print("File already exists. Skip create file") 
+        print("File already exists. Skip create file")
     else:
         subprocess.run(["touch", first_file], cwd=repo_directory)
     pass
@@ -61,52 +58,48 @@ def create_local_repo_file():
 
 def add_files_for_commit():
 
-    #stage file created
+    # stage file created
     subprocess.run(["git", "add", first_file], cwd=repo_directory)
     pass
 
+
 def commit_files():
 
-    #commit file
+    # commit file
     subprocess.run(["git", "commit", "-m", msg], cwd=repo_directory)
 
     pass
 
 
-#--------------- remote setup ---------------#
-
 def create_github_repo():
 
+    # generate data for request, set repo to private
+    repo_config = '{"name": "%s", "private": "true"}' % repo_name
 
-    #generate data for request, set repo to private
-    repo_config = '{"name": "%s", "private": "true"}' %repo_name
-
-    #create repo
+    # create repo
     if auth_type == 'token':
         header = 'Authorization: token ' + access_token
         response = subprocess.run(["curl", "--header", header, "--data", repo_config, "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
-    
+
     else:
         response = subprocess.run(["curl", "--user", github_name, "--data", repo_config, "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
 
-    #confirm repo now exists under user
+    # confirm repo now exists under user
     if auth_type == 'token':
         response = subprocess.run(["curl", "--header", header, "--request", "GET", "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
-    
+
     else:
         response = subprocess.run(["curl", "--user", github_name, "--request", "GET", "https://api.github.com/user/repos"], check=True, stdout=subprocess.PIPE)
-    
-    #convert from completed process for easier parsing
-    response_json = json.loads(response.stdout.decode("utf-8"))
-    print("%d repos by user" %len(response_json))
 
-    #confirm repo is created and extract url
-    for repo_id in range(len(response_json)):        
+    # convert from completed process for easier parsing
+    response_json = json.loads(response.stdout.decode("utf-8"))
+    print("%d repos by user" % len(response_json))
+
+    # confirm repo is created and extract url
+    for repo_id in range(len(response_json)):
         remote_name = response_json[repo_id]['name']
-        remote_url = response_json[repo_id]['html_url'] 
-        # print(remote_name) 
-        # print(remote_url) 
-    
+        remote_url = response_json[repo_id]['html_url']
+
         if(remote_name == repo_name):
             print("Repo now created on github")
             break
@@ -114,24 +107,22 @@ def create_github_repo():
     return remote_url
 
 
-#--------------- link local and remote ---------------#
-
 def add_remote_repo_url(remote_url):
 
     print(remote_url)
-    #url for repo
+    # url for repo
     server = remote_url + ".git"
-    print("server: %s" %server)
-    
+    print("server: %s" % server)
+
     if auth_type == 'token':
-        server = server.replace("//", "//%s:%s@" %(github_name, access_token))
+        server = server.replace("//", "//%s:%s@" % (github_name, access_token))
 
     else:
-        server = server.replace("//", "//%s@" %(github_name))
+        server = server.replace("//", "//%s@" % (github_name))
 
-    print("server: %s" %server)
+    print("server: %s" % server)
 
-    #set origin
+    # set origin
     subprocess.run(["git", "remote", "set-url", "origin", server], cwd=repo_directory)
 
     pass
@@ -140,15 +131,14 @@ def add_remote_repo_url(remote_url):
 
 def push_local_repo_to_remote(server):
 
-
-    #insert username to origin url
+    # insert username to origin url
     push_url = server
-    print("push url: %s" %push_url)
+    print("push url: %s" % push_url)
 
-    #push to server
+    # push to server
     subprocess.run(["git", "push", "-u", push_url, "master"], cwd=repo_directory)
     pass
-    
+
 
 def remote_auth_option():
     print("Select authentication type to github")
@@ -164,20 +154,8 @@ def remote_auth_option():
     pass
 
 
-# def read_token_from_file():
-
-#     TOKEN_LEN = 40
-    
-#     #read token from file
-#     with open('token', 'r') as token_file:
-#         token = token_file.read(TOKEN_LEN)
-#         print("Access token found: %s" %token)
-
-#     return token
-
-
 def start_program_flow():
-    
+
     print("------ Start ------")
 
     remote_auth_option()
@@ -199,11 +177,5 @@ def start_program_flow():
     pass
 
 
-def main():
-    start_program_flow()
-
 if __name__ == "__main__":
-    main()
-
-
-#End of file
+    start_program_flow()
