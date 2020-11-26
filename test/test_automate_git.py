6583ed74bdb37ef81468ac368f72a777e3ebb624
Luke Stedman <luke.stedman@gmail.com>
2020-11-26 18:48:16 +0000

    Added comments as requested

--- test/test_automate_git.py
+++ test/test_automate_git.py
@@ -6,11 +6,11 @@ from pathlib import Path
 import git
 from py._builtin import execfile
 
-CONFIG_FILE = 'config.ini'
-BASE_CONFIG_KEY = 'your_settings'
-
 
 def generate_test_config():
+    """
+    Helper function to generate a sample config.ini file for use in the application
+    """
     repo_name = 'test_repo'
     directory = '/tmp'
     try:
@@ -19,31 +19,49 @@ def generate_test_config():
         pass
 
     config = ConfigParser()
-    config[BASE_CONFIG_KEY] = {}
-    config[BASE_CONFIG_KEY]['github_token'] = "asdasd"
-    config[BASE_CONFIG_KEY]['repo_name'] = repo_name
-    config[BASE_CONFIG_KEY]['commit_file'] = "touched"
-    config[BASE_CONFIG_KEY]['first_commit_msg'] = "Initial commit"
-    config[BASE_CONFIG_KEY]['github_name'] = "test"
-    config[BASE_CONFIG_KEY]['directory'] = directory
-    config[BASE_CONFIG_KEY]['cmd'] = 'touch another_file'
-    with open(CONFIG_FILE, 'w') as fh:
+    base_config_key = 'your_settings'
+    config[base_config_key] = {}
+    config[base_config_key]['github_token'] = "asdasd"
+    config[base_config_key]['repo_name'] = repo_name
+    config[base_config_key]['commit_file'] = "touched"
+    config[base_config_key]['first_commit_msg'] = "Initial commit"
+    config[base_config_key]['github_name'] = "test"
+    config[base_config_key]['directory'] = directory
+    config[base_config_key]['cmd'] = 'touch another_file'
+
+    config_file = 'config.ini'
+    with open(config_file, 'w') as fh:
         config.write(fh)
-    return config
 
 
+# The config file must be generated before the application is loaded to ensure the data
+# is loaded by the application
 generate_test_config()
-
 execfile('../automate-git.py')
 
 
 def mock_json_loads(response_output):
+    """
+    Mocking function for json.loads(), returns a basic dictionary representing the data
+    returned from github.
+
+    :param str response_output: String output that is passed to json.loads(), is ignored
+    in this case
+    :return: Dictionary containing the bear minimum github response
+    """
     return [{'name': 'test_repo', 'html_url': 'http://github.com/fake_repo'}]
 
 
 def test_program_flow(monkeypatch):
+
+    # Apply the mocking on the json.loads() method
     monkeypatch.setattr(json, 'loads', mock_json_loads)
+
+    # Run the main application flow
     start_program_flow()
+
+    # Get a path object for the repo defined in the config and create a git repo object
+    # for further testing
     repo = Path('/tmp/test_repo')
     git_repo = git.Repo(repo)
 
